49e67e38c1af0bd930953b66d8337e2a9d62da60
古舞　千暁 <185x116x@istc-a-004.epc.center.kobe-u.ac.jp>
2018-04-17 14:54:28 +0900

    ex05~08 commit

---
+++ HelloJavaScript/vec3.js
@@ -0,0 +1,22 @@
+// Constructor
+Vec3 = function( x, y, z )
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+// Add method
+Vec3.prototype.add = function( v )
+{
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+
+// sum method
+Vec3.prototype.sum = function()
+{
+    return this.x + this.y + this.z;
+}
